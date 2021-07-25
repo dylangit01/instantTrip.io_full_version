824c8efebc7357ab8c949f5d8c48e9ba2588c05c
Dylan <dcren2010@gmail.com>
2021-07-25 00:15:21 -0400

    signup hasnt done yet

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+import { LockOutlined } from '@material-ui/icons';
+import useStyles from './styles';
 
 const Auth = () => {
-	return (
-		<div>
-			AUTH
-		</div>
-	)
-}
+	const classes = useStyles();
 
-export default Auth
+	const isSignup = false;
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	return (
+		<Container component='main' maxWidth='xs'>
+			<Paper className={classes.paper} elevation={3}>
+				<Avatar className={classes.avatar}>
+					<LockOutlined />
+				</Avatar>
+				<Typography component='h1' variant='h5'>
+					{isSignup ? 'Sign Up' : 'Sign In'}
+				</Typography>
+				<form className={classes.form} onSubmit={e => handleSubmit(e)}>
+
+				</form>
+			</Paper>
+		</Container>
+	);
+};
+
+export default Auth;
