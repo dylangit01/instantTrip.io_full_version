4349e4b52582d0d43822b454d6bb6c05a71f569c
Dylan <dcren2010@gmail.com>
2021-07-27 12:39:56 -0400

    delete unused file

--- client/src/redux/actions/posts.js
+++ client/src/redux/actions/posts.js
@@ -4,7 +4,7 @@ export const FETCH_ALL = 'FETCH_ALL';
 export const CREATE_POST = 'CREATE_POST';
 export const UPDATE_POST = 'UPDATE_POST';
 export const DELETE_POST = 'DELETE_POST';
-export const LIKE_POST = 'LIKE_POST'
+export const LIKE_POST = 'LIKE_POST';
 
 export const GET_ID = 'GET_ID';
 export const CLEAR_ID = 'CLEAR_ID';
