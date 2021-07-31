08fc028e34c25e5b77cf1d97db7d8312a353b106
Dylan <dcren2010@gmail.com>
2021-07-31 14:04:33 -0400

    deploy server on heroku

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // "posts" must match with server router for posts, and NEVER PUT "/" after endpoint
-const endpoint = 'http://localhost:5000/posts';
-// const endpoint = 'https://instant-trip.herokuapp.com/posts';
+// const endpoint = 'http://localhost:5000/posts';
+const endpoint = 'https://instant-trip-full-verison.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(endpoint);
 
