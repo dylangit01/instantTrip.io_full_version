bfb5864c1e51d9daa49ed4c5bad703561506f366
Dylan <dcren2010@gmail.com>
2021-08-03 10:28:48 -0400

    add search testField

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -13,24 +13,28 @@ import { getPosts } from '../../redux/actions/posts';
 
 // Create below function to know which page we are currently on, and what search term we are looking for
 const useQuery = () => {
-	return new URLSearchParams(useLocation().search)
-}
+	return new URLSearchParams(useLocation().search);
+};
 
 const Home = () => {
 	const classes = useStyles();
 
 	const dispatch = useDispatch();
 
+	// My search function
 	const [searchField, setSearchField] = useState('');
-
 	const [showAddPost, setShowAddPost] = useState(false);
+
+	// JSM search function
+	const [search, setSearch] = useState('');
+	const [tags, setTags] = useState([]);
 
 	const query = useQuery();
 	const history = useHistory();
 
 	// This is going to read the url and see if we have a page parameter in there, if we dont have that page, it much be on page 1
 	const page = query.get('page') || 1;
-	const searchQuery = query.get('searchQuery')
+	const searchQuery = query.get('searchQuery');
 
 	// Using dispatch in useEffect to dispatch an action to get the posts data, don't forget the "()" for getPosts
 	useEffect(() => {
@@ -40,28 +44,55 @@ const Home = () => {
 	const posts = useSelector((state) => state.posts);
 	const postID = useSelector((state) => state.postID);
 
-	// Search function
+	// My Search function
 	const searchedPosts = posts.filter((post) => {
 		const combineSearch = `${post.creator} ${post.title} ${post.tags} ${post.description} ${post.name}`;
 		return combineSearch.toLowerCase().includes(searchField.toLowerCase());
 	});
 
+	// JSM Search function
+	const handleKeyPress = (e) => {
+		if (e.keyCode === 13) {
+		}
+	};
+
+	const handleAdd = tag => setTags([...tags, tag])
+	const handleDelete = tagToDelete => setTags(tags.filter(tag => tag !== tagToDelete));
+
 	return (
 		<Grow in>
 			<Container maxWidth='xl'>
-				<Grid className={classes.gridContainer} container justifyContent='space-between' alignItems='stretch' spacing={6}>
+				<Grid
+					className={classes.gridContainer}
+					container
+					justifyContent='space-between'
+					alignItems='stretch'
+					spacing={6}
+				>
 					<Grid item xs={12} sm={6} md={4}>
-
-					<AppBar className={classes.appBarSearch} position='static' color='inherit'>
+						<AppBar className={classes.appBarSearch} position='static' color='inherit'>
 							<TextField
 								name='search'
 								variant='outlined'
 								label='Search Post'
 								fullWidth
-								value='TEST'
-								onChange={() => {}}
+								value={search}
+								onChange={(e) => setSearch(e.target.value)}
+								onKeyPress={handleKeyPress}
 							/>
-					</AppBar>
+
+							{/* Todo: install material-ui-chip-input */}
+							<TextField
+								style={{ marginTop: '10px' }}
+								variant='outlined'
+								label='Search Tags'
+								fullWidth
+								value={tags}
+								onAdd={handleAdd}
+								onDelete={handleDelete}
+							/>
+
+						</AppBar>
 
 						<Paper className={classes.searchBar} elevation={6}>
 							<SearchBar setSearchField={setSearchField} />
