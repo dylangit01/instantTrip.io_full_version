589f44ca287b0d85f8fe73249814cdc6f9533e94
Dylan <dcren2010@gmail.com>
2021-08-05 01:04:53 -0400

    finding bug 01

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -25,6 +25,8 @@ API.interceptors.request.use((req) => {
 // export const fetchPosts = () => API.get('/posts');
 export const fetchPosts = (page) => API.get(`/posts?page=${page}`);
 
+export const fetchPost = (id) => API.get(`/posts/${id}`);
+
 export const createPost = (newPost) => API.post('/posts', newPost);
 export const updatePost = (id, updatedPost) => API.patch(`/posts/${id}`, updatedPost);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
