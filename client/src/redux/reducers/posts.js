e5ae46aa0426ca8cc2930b63fdf674a15e09843a
Dylan <dcren2010@gmail.com>
2021-08-05 01:27:27 -0400

    finging bug05

--- client/src/redux/reducers/posts.js
+++ client/src/redux/reducers/posts.js
@@ -1,12 +1,12 @@
-import { FETCH_ALL, CREATE_POST, UPDATE_POST, DELETE_POST, LIKE_POST, SEARCH_POST, START_LOADING, END_LOADING } from '../actions/posts';
+import { FETCH_ALL, FETCH_SINGLE_POST, CREATE_POST, UPDATE_POST, DELETE_POST, LIKE_POST, SEARCH_POST, START_LOADING, END_LOADING } from '../actions/posts';
 
 	// It's very difficult to find typo error in reducers: action.payload not "action.post"
 const posts = (state = {isLoading:true, posts: [] }, action) => {
 	switch (action.type) {
 		case START_LOADING:
-			return { ...state, isLoading: true }
+			return { ...state, isLoading: true };
 		case END_LOADING:
-			return {...state, isLoading: false}
+			return { ...state, isLoading: false };
 		case FETCH_ALL:
 			return {
 				...state,
@@ -15,8 +15,10 @@ const posts = (state = {isLoading:true, posts: [] }, action) => {
 				currentPage: action.payload.currentPage,
 				numberOfPages: action.payload.numberOfPages,
 			};
+		case FETCH_SINGLE_POST:
+			return { ...state, post: action.payload };
 		case SEARCH_POST:
-			return {...state, posts: action.payload};
+			return { ...state, posts: action.payload };
 		case CREATE_POST:
 			// Before was [...posts, action.payload], since it changes to object, so as below
 			return { ...state, posts: [...state.posts, action.payload] };
@@ -24,10 +26,10 @@ const posts = (state = {isLoading:true, posts: [] }, action) => {
 		case LIKE_POST:
 			// since likePost has same logic with update, so the case can be put together
 			// Here the payload is the data return from server, so the id is "_id" from mongoDB
-			return {...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post))};
+			return { ...state, posts: state.posts.map((post) => (post._id === action.payload._id ? action.payload : post)) };
 		case DELETE_POST:
 			// here payload is the "id", because action return the payload is "id"
-			return {...state, posts: state.posts.filter((post) => post._id !== action.payload)};
+			return { ...state, posts: state.posts.filter((post) => post._id !== action.payload) };
 		default:
 			return state;
 	}
